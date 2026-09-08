1e3d05c0a1b8eda240af8fdac3762d263ea18644
chanderquichiz <chanderquichiz@outlook.com>
2026-09-07 17:26:33 -0700

    refinaciones en el contrato

--- services/catalog/src/product/product.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-
-@Injectable()
-export class ProductService {
-    constructor(prismaService: PrismaService) {}
-  create(product: CreateProductDto, file: Express.Multer.File) {
-  }
-}
